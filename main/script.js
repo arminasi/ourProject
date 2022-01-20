2327ea7493f85cdd55e6d10d89d399c34f93e3ca
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2022-01-20 18:36:36 +0400

    feat: adding form to first page

--- main/script.js
+++ main/script.js
@@ -3,22 +3,21 @@ function openForm() {
 }
 
 function closeForm() {
-    document.getElementById("popupForm").style.display = "none";
+  document.getElementById("popupForm").style.display = "none";
 }
 
 var bookStyles = {
   color: "red",
   backgroundColor: "blue",
   height: "300px",
-  width: "200px"
+  width: "200px",
 };
 
-
 for (let style in bookStyles) {
-div.style[style] = bookStyles[style];
+  div.style[style] = bookStyles[style];
 }
 
- body.appendChild(div);
+body.appendChild(div);
 
 function addItem() {
   document.createElement("div");
