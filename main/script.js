66cb8360bd465b291d48920d21ef1c05947f4fc1
arminasi <armanminasyanc@gmail.com>
2022-02-14 11:01:11 +0400

    was added correct input field

--- main/script.js
+++ main/script.js
@@ -1,12 +1,34 @@
-function openForm() {
-  if(document.getElementById("popupForm").style.display === "block") {
+const main = document.getElementById("mod");
+const popUpForm = document.getElementById("popupForm");
+const openFormBtn = document.getElementById("myBtn");
+
+let inputVal = document.getElementById("inputValue")
+const addBtn = document.getElementById("buttonForm");
+const root = document.getElementById("lessonsContainerParent");
+
+document.body.addEventListener("click", function() {
+  openFormBtn.addEventListener("click", function() {
+    if(document.getElementById("popupForm").style.display === "block") {
+      document.getElementById("popupForm").style.display = "none";
+      document.getElementById("myBtn").innerHTML = "Add";
+    } else {
+      document.getElementById("popupForm").style.display = "block";
+      document.getElementById("myBtn").innerHTML = "Close";
+    }
+  })
+  popUpForm.style.display = "none";
+  document.getElementById("myBtn").innerHTML = "Add";
+
+}, {capture: true})
+
+popUpForm.addEventListener("click", function() {
+  popUpForm.style.display = "block";
+  if(document.getElementById("popupForm").style.display != "block") {
     document.getElementById("popupForm").style.display = "none";
     document.getElementById("myBtn").innerHTML = "Add";
-  } else {
-    document.getElementById("popupForm").style.display = "block";
-    document.getElementById("myBtn").innerHTML = "Close";
   }
-}
+    document.getElementById("myBtn").innerHTML = "Close";
+})
 
 function changeMod() {
   let form = document.getElementById("container");
@@ -15,10 +37,6 @@ function changeMod() {
   form.style.background = "black";
   form.style.color = "white";
 }
-
-let inputVal = document.getElementById("inputValue")
-const addBtn = document.getElementById("buttonForm");
-const root = document.getElementById("lessonsContainerParent");
 
 addBtn.addEventListener("click", function() {
 const root = document.getElementById("lessonsContainerParent");
@@ -47,9 +65,13 @@ const root = document.getElementById("lessonsContainerParent");
   LIT.appendChild(LITa);
   LITa.innerHTML = document.getElementById("inputValue").value
   if(LITa.innerHTML === "") {
-    alert("Lesson name field is empty")
+    return;
   } else {
     root.appendChild(LC);
+    LPH.innerText = inputVal.value[0].toUpperCase();
+    LPH.firstChild.style.marginTop = "25%"
   }
   document.getElementById("inputValue").placeholder = "JavaScript";
+  document.getElementById("inputValue").innerText = inputVal.value;
+  inputVal.value = ""
 })
